26b1c27b023ef8304296fa1b87ba16c928d1439c
TristanB12 <tristan.bourgeois@epitech.eu>
2021-02-03 13:18:44 +0400

    login page and header bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
+    <HeaderBar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import HeaderBar from '@/components/HeaderBar.vue';
+    export default {
+        components: {
+          HeaderBar,
+        },
+    }
+</script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap');
 
 * {
   box-sizing: border-box;
-  margin: 0;
   padding: 0;
+  margin: 0;
 }
-
 .router-link-exact-active {
-  text-decoration:  underline;
+  text-decoration: underline;
+}
+a {
+  color: white;
+  text-decoration: none;
 }
 </style>
---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header-container">
+        <div id="headerBar">
+            <h1>PostApp</h1>
+            <ul id="navBar">
+                <li>Home</li>
+                <li><router-link to="Login">Login</router-link></li>
+                <li>Signup</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'headerBar'
+    }
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+    background-color: #45AAF2;
+}
+h1 {
+    font-family: 'Lobster', cursive;
+    font-size: 2.1em;
+}
+#headerBar {
+    width: 70%;
+    height: 8vh;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    color: white;
+    align-items: center;
+
+    ul {
+        width: 30%;
+        display: flex;
+        justify-content: space-between;
+        list-style: none;
+
+        li {
+            font-size: 1.5em;
+            font-weight: 400;
+            font-family: 'Roboto', sans-serif;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+}
+</style>
